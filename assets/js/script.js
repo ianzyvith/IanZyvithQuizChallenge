1244259a9e56d9628139178eeace841b63e231a5
ianzyvith <izyvith25@gmail.com>
2022-06-02 19:50:55 -0400

    added end game and timer functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,21 @@ var questions = [
   
   var questionIndex = 0;
   var correctCount = 0;
+  var time = 60;
+  var timer;
+
+
+
+  // starts the quiz when text is clicked
+  function startQuiz() {
+    timer = setInterval(timerStart, 1000);
+    timerEl.textContent = "Time: " + time;
+
+
+    renderQuestion();
+  }
+    
+  
   
   // generates questions
   function renderQuestion() {
@@ -37,6 +52,8 @@ var questions = [
     optionListEl.addEventListener("click", checkAnswer);
   }
 
+
+
   // checks answer from question
   function checkAnswer(event) {
 
@@ -51,20 +68,53 @@ var questions = [
         questionResultEl.textContent = "That is Incorrect. You have lost 5 seconds.";
     }
 
+    optionListEl.innerHTML = "";
     nextQuestion();
   }
   
+
+
   // brings up next question
   function nextQuestion() {
     questionIndex++;
+
     // when all question are asked end quiz
-    renderQuestion();
+    if (questionIndex === questions.length) {
+        clearInterval(timer);
+        endGame();
+    }
+
+    else {
+        renderQuestion();
+    }
   }
 
-  // starts the quiz when text is clicked
-  function startQuiz() {
-    startBtn.textContent = "Press to Start Quiz";
+  // timer function
+  function timerStart() {
+      time--;
+      timerEl.textContent = "Time: " + time;
+
+      if (time <= 0) {
+        timerEl.textContent = "0";
+        endGame();
+      }
   }
 
-  startQuiz();
-  startBtn.addEventListener("click", renderQuestion);
+  // end of game function
+  function endGame() {
+
+    optionListEl.innerHTML = "";
+
+    questionEl.innerHTML = "Thank you for taking the quiz!";
+        
+    var finalScore = document.createElement("p");
+    finalScore.textContent = "Your final score is " + correctCount + "!";
+    questionEl.appendChild(finalScore);
+
+  }
+
+
+
+  // calls to start
+  startBtn.addEventListener("click", startQuiz);
+  startBtn.textContent = "Press to Start Quiz";
